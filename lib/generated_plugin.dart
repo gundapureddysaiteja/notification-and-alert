a2da918f45459bdb187282b959c33ca7130c1687
gundapureddysaiteja <116089043+gundapureddysaiteja@users.noreply.github.com>
2022-12-10 15:46:41 +0530

    Add files via upload

---
+++ lib/generated_plugin.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins(Registrar, registrar) {
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
